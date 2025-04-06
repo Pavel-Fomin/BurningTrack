fab12be8921e55ad6217cadddce8a102104b3217
Pavel Fomin <pavelfomin@icloud.com>
2025-04-07 00:16:50 +0300

    Initial Commit

---
+++ TrackList/TrackListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackListApp.swift
+//  TrackList
+//
+//  Created by Pavel Fomin on 07.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrackListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrackListTests/TrackListTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackListTests.swift
+//  TrackListTests
+//
+//  Created by Pavel Fomin on 07.04.2025.
+//
+
+import Testing
+@testable import TrackList
+
+struct TrackListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
